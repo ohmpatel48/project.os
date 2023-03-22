726c064974b67f641d02d2be8b63b7ff1295b563
MahilKuvadiya <127921738+MahilKuvadiya@users.noreply.github.com>
2023-03-22 07:40:40 +0000

    comments added

--- frontend/main/src/app/pages/mru/mru.component.ts
+++ frontend/main/src/app/pages/mru/mru.component.ts
@@ -15,15 +15,15 @@ export class MruComponent {
   pages: any=[];
   summary:any;
   output:any;
-  tf:any;
-  tp:any;
-  sp:any;
+  tf:any;     //total frames
+  tp:any;     //total pages
+  sp:any;     //sequence of pages
   observation:any;
-  tr:any;
-  nh:any;
-  nf:any;
-  hr:any;
-  fr:any;
+  tr:any;     //total replacement
+  nh:any;     //no. of hits
+  nf:any;     //no. of frames
+  hr:any;     //hit ratio
+  fr:any;     //fault ration
   savedata:any;
   keys:any = [];
   values:any = [];
@@ -43,9 +43,11 @@ export class MruComponent {
     let n =this.pages.length;
     this.savedata = {frames:frames,array:this.pages};  
     this.setdata.savemru(this.savedata).subscribe((data)=>{console.log(data)},(error)=>console.log(error));
+    
+    //maintainng the arrays for "hit" and "Replace columnns"
     let hit =[];
-    let inst =[];
     let v =[];
+    let inst =[];
     for (let index = 0; index < this.pages.length; index++) {
       hit[index] = "No";  
       v[index] = "-";
@@ -53,29 +55,54 @@ export class MruComponent {
       //     inst[index][j]="-";   
       // }
     }
+
+    //maintainnig an array for table
     let mentian = [];
     for (var i = 0; i < n; i++) {
       let temp3=[];
+
+      //will get the index of current pages int the inst array
       var idx = inst.indexOf(this.pages[i]);
+
+      //will go in if , if there is a miss
       if (idx == -1) {
+
+          //will go in if , if there is a empty space for a page
           if (inst.length < frames) {
               // inst.push(pages[i]);
               inst.unshift(this.pages[i]);
           }
-          else {             
+
+          //will go in else , if a page replacement is required
+          else {
+            
+              //updating the "Replace" column
               v[i] = inst[0];
               inst.splice(0,1);
               inst.unshift(this.pages[i]);
           }
+
+          //maintaining the number of miss 
           this.misscount++;
       }
+
+      //will go in else , if a hit occur
       else {
          
+          //will swap the page and keep the MRU page at 0th index
           inst.splice(inst.indexOf(this.pages[i]), 1);
           inst.unshift(this.pages[i]);
+
+          //update the "hit" column 
           hit[i] = "Yes";
+
+          //maintaining the number of hits
           this.hitcount++;
+
       }
+
+
+      //updating the temp array if the size of temp3 != frames
       for (let k = 0; k < frames; k++) {
         if (inst[k] == undefined) {
           temp3[k] = "-";
@@ -83,9 +110,12 @@ export class MruComponent {
           temp3[k] = inst[k];
         }
       }
-      
+
+      //pushing the temp3 array into the mantian array
       mentian.push(temp3);   
   }
+
+  //After computing all the hits and misses, we are storing all the values into a table
   for (let index = 0; index < this.pages.length; index++) {
     let temp:any = {};
     let tempstring = "P"+ count;
@@ -99,6 +129,8 @@ export class MruComponent {
     temp[`Replaced`] = v[index]; 
     this.tableData.push(temp);
   }
+
+
   this.getData();
   this.summary="Summary:-";
   this.output = "Output";
